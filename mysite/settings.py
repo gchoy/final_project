1ab9f1cfe9618b6ec8422810171247246bec4cbd
gchoy <gabriela.choy@gmail.com>
2017-05-07 02:46:27 -0700

    changed settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'finalproj',
-        'USER': 'gabriela',
+        'USER': 'super',
+        'HOST': 'gchoy-427.postgres.pythonanywhere-services.com',
+        'PORT': '10427',
+
+
     }
 }
 # Password validation
